35fc246882a6f55d4415957c8c49043ea5ac24f0
selim <selim.mht@gmail.com>
2021-04-04 23:46:26 +0200

    Add files via upload

---
+++ splitter-app/splitter-app/ContentView.swift
@@ -0,0 +1,69 @@
+//
+//  ContentView.swift
+//  splitter-app
+//
+//  Created by selimmh on 04.04.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 0
+    @State private var tipPercentage = 2
+    
+    let tipPercentages = [10,15,20,25,0]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+        
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
+    var body: some View {
+        
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2 ..< 100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                
+                Section {
+                    
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0 ..< tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section (header: Text("How much tip do you want to leave?")){
+                    Text("$\(totalPerPerson)")
+                }
+            }
+            .navigationBarTitle("Splitter")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ splitter-app/splitter-app/splitter_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  splitter_appApp.swift
+//  splitter-app
+//
+//  Created by selimmh on 04.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct splitter_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
